841ea3736087eb36f1372b69ff175af87490d575
mikediniz <mikediniz.system@gmail.com>
2023-03-09 21:40:20 -0300

    Teste VsCode

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -6,6 +6,7 @@ public class PrimeiroPrograma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Teste Vs Code
 		/*int a = 2;
 		int b = 3;
 		System.out.println("Hello World! " + (a+b));*/
